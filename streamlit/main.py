1d6035e3533e2829f44f6f8d57dbf56396b16319
Flora Serafim de Carvaho <134112749+florascarvalho@users.noreply.github.com>
2025-02-16 16:01:23 -0300

    Add files via upload

---
+++ streamlit/main.py
@@ -0,0 +1,22 @@
+import streamlit as st
+
+
+st.title("FASE 4 - Trabalho Final")
+
+st.markdown("""
+### 🏆 Tech Challenge da Fase 4 da Pós-Tech FIAP
+""")
+
+st.markdown("""
+### 🔗 Aluna: Flora Serafim de Carvalho | RM: RM354000
+""")
+
+
+st.write("""
+O desafio consiste em analisar dados históricos do preço do petróleo Brent, disponíveis no site do IPEA, e criar um dashboard interativo que apresente pelo menos quatro insights relevantes sobre a variação dos preços, considerando fatores como crises econômicas e demanda global.
+
+Além disso, é necessário desenvolver um modelo de Machine Learning para prever o preço do petróleo diariamente, utilizando time series forecasting. O modelo e os insights devem estar alinhados em um storytelling. Também é obrigatório criar um plano de deploy e disponibilizar um MVP funcional no Streamlit. Por fim, um vídeo de até 5 minutos deve ser produzido para apresentar o desenvolvimento do projeto.
+
+""")
+
+st.sidebar.success("🎓Aluna: Flora Serafim de Carvalho | RM: RM354000")
